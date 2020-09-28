7b914704de9ad336df0a27a39920551011ad81c1
matiasbn <matias.barriosn@gmail.com>
2020-09-27 21:20:51 -0300

    deploying to mainnet

---
+++ README.md
@@ -0,0 +1,27 @@
+# Beba token
+```
+Nombre: Beba
+Abreviacion: Beb
+Emision: 120 millones
+```
+
+## Deployment:
+
+Crear archivo ```.secret``` con mnemonico.
+
+Crear archivo ```.ìnfurakey``` con api key de Infura.
+
+```
+npx oz compile
+npx oz deploy
+```
+
+### Rinkeby address
+```
+0xd5dda092e891d1a4ca5c5c66946072753725f67e
+```
+
+### Mainnet address
+```
+0xd5dda092e891d1a4ca5c5c66946072753725f67e
+```
--- truffle-config.js
+++ truffle-config.js
@@ -75,6 +75,19 @@ module.exports = {
       confirmations: 2, // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true // Skip dry run before migrations? (default: false for public nets )
+    },
+    mainnet: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://mainnet.infura.io/v3/${infuraKey}`
+        ),
+      network_id: 1,
+      // gas: 5500000,
+      gasPrice: 40000000000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: false
     }
     // Useful for private networks
     // private: {
